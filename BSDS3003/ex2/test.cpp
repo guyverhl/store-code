51fe9ee0022ee91326cc5d19691b0c05b9fdcdf7
Guyver Chan <guyverchan@benovelty.com>
2021-03-05 17:04:28 +0800

    add test.cpp

---
+++ BSDS3003/ex2/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    // test 1 single (
+    /* test 2 {
+    [ test 3 }
+    */ cout << "Hello World:)" << "\n";
+    cout << '[' << endl;
+    int i = 3 * 4;
+
+}
